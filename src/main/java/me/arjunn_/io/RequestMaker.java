09a6f6c4d2480636283e461638d2eea811333d02
ArjunnMC <49657835+ArjunnMC@users.noreply.github.com>
2019-05-30 16:21:06 -0400

    initial commit

---
+++ src/main/java/me/arjunn_/io/RequestMaker.java
@@ -0,0 +1,52 @@
+package me.arjunn_.io;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+public class RequestMaker extends Thread {
+
+    static BufferedReader in;
+    Server server;
+
+    public RequestMaker(Server server) {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+
+        while (true) {
+            try {
+                String send = in.readLine();
+                System.out.println("Got input " + send);
+                JSONObject request = new JSONObject();
+                request.put("type", "request");
+                request.put("event", "echo");
+                request.put("data", send);
+                request.put("responseID", UUID.randomUUID().toString());
+                System.out.println(request.getString("responseID"));
+
+                try {
+                    System.out.println("Sending messages to " + Server.servers.keySet().toString());
+                    for (String serverName : Server.servers.keySet()) {
+                        long start = System.currentTimeMillis();
+                        System.out.println("SENDING TO " + serverName);
+                        String response = server.getStringFromClient(serverName, request).get(20, TimeUnit.MILLISECONDS);
+                        System.out.println(response);
+                        System.out.println("Response time: " + (System.currentTimeMillis() - start) + " milliseconds.");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/me/arjunn_/io/Server.java
@@ -0,0 +1,102 @@
+package me.arjunn_.io;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+
+public class Server {
+
+    protected static HashMap<String, ServerThread> servers = new HashMap<>();
+    private static HashMap<String, CompletableFuture<String>> futuresToResolve = new HashMap<>();
+
+    static final int PORT = 9093;
+
+    public Server() {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+
+        try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while (true) {
+            try {
+                socket = serverSocket.accept();
+                System.out.println("Got a new connection on " + socket.getInetAddress());
+            } catch (IOException e) {
+                System.out.println("I/O error: " + e);
+            }
+            // new thread for a client
+            new ServerThread(socket).start();
+
+            new RequestMaker(this).start();
+
+        }
+    }
+
+    public static void main(String args[]) {
+        new Server();
+    }
+
+    public static void addServer(String server, ServerThread thread) {
+        servers.put(server, thread);
+        System.out.println("Client has connected: " + servers.keySet().toString());
+    }
+
+    public static void removeClient(ServerThread thread) {
+        servers.values().remove(thread);
+        System.out.println("Client has disconnected: " + servers.toString());
+    }
+
+    public CompletableFuture<String> getStringFromClient(String client, JSONObject request) {
+        CompletableFuture<String> response = new CompletableFuture<>();
+        futuresToResolve.put(request.getString("responseID"), response);
+
+        servers.get(client).sendData(request);
+
+        return response;
+
+    }
+
+    public static void handle(JSONObject sent, ServerThread thread) {
+
+        System.out.println(sent);
+        String type = sent.getString("type");
+        String event = sent.getString("event");
+        String responseID = sent.getString("responseID");
+
+
+        if (sent.has("responseID") && type.equals("response")) {
+            if (futuresToResolve.containsKey(sent.getString("responseID"))) {
+                futuresToResolve.get(sent.getString("responseID")).complete(sent.getString("data"));
+            }
+        }
+
+        if (event.equalsIgnoreCase("connect")) {
+            addServer(sent.getString("name"), thread);
+            JSONObject ack = new JSONObject();
+            ack.put("type", "response");
+            ack.put("event", "connect");
+            ack.put("status", "connected");
+            ack.put("responseID", "000");
+            thread.sendData(ack);
+        } else if (event.equalsIgnoreCase("disconnect")) {
+            thread.disconnect();
+        } else if (event.equalsIgnoreCase("echo")) {
+            JSONObject response = new JSONObject();
+            response.put("type", "response");
+            response.put("event", "echo");
+            response.put("data", sent.getString("data"));
+            response.put("responseID", sent.getString("responseID"));
+            thread.sendData(response);
+        }
+
+    }
+
+}
